95d31c5c1d0f68880a102043402eb6c95fea8335
LalitthaRavi <lalitha.rgs@gmail.com>
2024-08-06 06:49:36 +0800

    REquireemnts are added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'cell_segmentation',
+    version='0.0.0',
+    author = 'Lalitha Ravi',
+    author_email = 'lalitha.rgs@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
