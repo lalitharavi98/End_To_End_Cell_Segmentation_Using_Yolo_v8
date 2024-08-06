7cd5bd5ad457053f75cac10cb7e24ef201a003b9
LalitthaRavi <lalitha.rgs@gmail.com>
2024-08-06 11:40:11 +0800

    completed training_pipeline

--- app.py
+++ app.py
@@ -1,3 +1,24 @@
-from cell_segmentation.logger import logging
+import sys,os
+from cell_segmentation.pipeline.training_pipeline import TrainPipeline
+# from cell_segmentation.utils.main_utils import decodeImage, encodeImageIntoBase64
+# from flask import Flask, request, jsonify, render_template,Response
+# from flask_cors import CORS, cross_origin
+# from cell_segmentation.constant.application import APP_HOST, APP_PORT
 
-logging.info("Welcome to the custom log")
+
+
+
+# app = Flask(__name__)
+# CORS(app)
+
+# class ClientApp:
+#     def __init__(self):
+#         self.filename = "inputImage.jpg"
+
+
+
+# @app.route("/train")
+# def trainRoute():
+obj = TrainPipeline()
+obj.run_pipeline()
+    # return "Training Successfull!!" 
--- cell_segmentation/constant/training_pipeline/__init__.py
+++ cell_segmentation/constant/training_pipeline/__init__.py
@@ -1,7 +1,7 @@
 ARTIFACTS_DIR: str = "artifacts"
 
 """
-Data Ingestion related constant start with DATA_INGESTION VAR NAME
+    Data Ingestion related constant start with DATA_INGESTION VAR NAME
 """
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 
@@ -13,7 +13,7 @@ DATA_DOWNLOAD_URL: str = "https://drive.google.com/file/d/17vDLJrk4iYtysocirKNCD
 
 
 """
-Data Validation realted contant start with DATA_VALIDATION VAR NAME
+    Data Validation realted contant start with DATA_VALIDATION VAR NAME
 """
 
 DATA_VALIDATION_DIR_NAME: str = "data_validation"
@@ -21,3 +21,14 @@ DATA_VALIDATION_DIR_NAME: str = "data_validation"
 DATA_VALIDATION_STATUS_FILE = 'status.txt'
 
 DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "valid", "test", "data.yaml"]
+
+"""
+    MODEL TRAINER related constant start with MODEL_TRAINER var name
+"""
+MODEL_TRAINER_DIR_NAME: str = "model_trainer"
+
+MODEL_TRAINER_PRETRAINED_WEIGHT_NAME: str = "yolov8s-seg.pt"
+
+MODEL_TRAINER_NO_EPOCHS: int = 1
+
+MODEL_TRAINER_BATCH_SIZE: int = 16
