9d0569c2d92a8be046de758294807fc1ae2ca116
LalitthaRavi <lalitha.rgs@gmail.com>
2024-08-06 10:29:13 +0800

    Added code for data ingestion

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # End_To_End_Cell_Segmentation_Using_Yolo_v8
+
+# Workflows
+
+1. constants
+2. entity
+3. components
+4. pipelines
+5. app.py
---
+++ cell_segmentation/entity/artifacts_entity.py
@@ -0,0 +1,15 @@
+# Return type of any function (Output)
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionArtifact:
+    data_zip_file_path:str
+    feature_store_path:str
+
+@dataclass
+class DataValidationArtifact:
+    validation_status: bool
+
+@dataclass
+class ModelTrainerArtifact:
+    trained_model_file_path: str
